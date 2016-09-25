f41921099c89d20e9955ad84dbd41b742279d183
John Prantalos <johnprantalos@gmail.com>
2016-09-25 10:12:32 +0300

    Request imgur images via https

--- includes/html_head.php
+++ includes/html_head.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
-<link 
-	href="http://bootswatch.com/flatly/bootstrap.min.css" 
+<link
+	href="https://bootswatch.com/flatly/bootstrap.min.css"
 	rel="stylesheet">
 <link href="assets/css/style.css" rel="stylesheet">
 <!-- Google Meta -->
@@ -13,28 +13,28 @@
 <meta property="og:url" content="<?php echo $url ?>" />
 <meta property="fb:app_id" content="1469392136712174" />
 <meta property="og:locale" content="el_GR" />
-<meta property="og:image" content="http://i.imgur.com/y5a2v1W.jpg" />
+<meta property="og:image" content="https://i.imgur.com/y5a2v1W.jpg" />
 <meta property="og:image:width" content="1920" />
 <meta property="og:image:height" content="1080" />
 <!-- Twitter Meta -->
 <meta name="twitter:site" content="@PandorianGR" />
 <meta name="twitter:card" content="A JP experiment" />
 <meta name="twitter:url" content="<?php echo $url ?>" />
-<meta name="twitter:image" content="http://i.imgur.com/y5a2v1W.jpg" />
+<meta name="twitter:image" content="https://i.imgur.com/y5a2v1W.jpg" />
 
-<meta name="description" 
+<meta name="description"
 	content="
 		PHP script που υπολογίζει τον τελικό Βαθμό Πτυχίου για το ΠΠΣ
 		των Μηχανικών Πληροφορικής T.E. του Τ.Ε.Ι. Στερεάς Ελλάδας
 	"
 >
-<meta property="og:description" 
+<meta property="og:description"
 	content="
 		PHP script που υπολογίζει τον τελικό Βαθμό Πτυχίου για το ΠΠΣ
 		των Μηχανικών Πληροφορικής T.E. του Τ.Ε.Ι. Στερεάς Ελλάδας
 	"
 >
-<meta name="twitter:description" 
+<meta name="twitter:description"
 	content="
 		PHP script που υπολογίζει τον τελικό Βαθμό Πτυχίου για το ΠΠΣ
 		των Μηχανικών Πληροφορικής T.E. του Τ.Ε.Ι. Στερεάς Ελλάδας
@@ -42,7 +42,7 @@
 >
 <meta name="author" content="JP" />
 
-<meta name="keywords" content="john, prantalos, teiste, teilam, τει, 
+<meta name="keywords" content="john, prantalos, teiste, teilam, τει,
 	στερεας, ελλαδας, Τ.Ε.Ι., Λαμίας, πληροφορικής" />
 
 <meta property="og:title" content="Teilam Grades - JP" />
